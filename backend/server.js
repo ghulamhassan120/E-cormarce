22b9bc7d160ff458ec031d252575ca6fcbcb0e27
ghulamhassan120 <ghulamhassan.dev6@gmail.com>
2026-08-17 12:06:23 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,8 @@ app.get('/', (req, res) => {
 // Port configuration
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+export default app
